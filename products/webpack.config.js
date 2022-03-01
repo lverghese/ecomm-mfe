87243b9fc53a4c16876bb3defe7b1c28f33b1d73
lverghese <loganverghese@gmail.com>
2022-02-28 19:58:36 -0500

    products displayed through webpack

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,13 @@
+const HtmlWebpaclPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
-
+    devServer: {
+        port: 8081
+    },
+    plugins: [
+        new HtmlWebpaclPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
